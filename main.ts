b5664903ac056b9dd56ff6168ced43514b5a8232
lioujj <34760803+lioujj@users.noreply.github.com>
2024-04-11 11:53:23 +0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,121 @@
+/**
+ * BMP180 block
+ */
+//% weight=100 color=#30b30c icon="\uf2dc" block="BMP180"
+namespace BMP180 {
+    let BMP180_I2C_ADDR = 0x77;
+
+    export enum valueType {
+        //% block="pressure(hPa)"
+        value1 = 1,
+        //% block="temperature(C)"
+        value2 = 2,
+        //% block="altitude(m)"
+        value3 = 3
+    }
+
+
+    function setreg(reg: number, dat: number): void {
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = dat;
+        pins.i2cWriteBuffer(BMP180_I2C_ADDR, buf);
+    }
+
+    function getreg(reg: number): number {
+        pins.i2cWriteNumber(BMP180_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BMP180_I2C_ADDR, NumberFormat.UInt8BE);
+    }
+
+    function getUInt16BE(reg: number): number {
+        pins.i2cWriteNumber(BMP180_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BMP180_I2C_ADDR, NumberFormat.UInt16BE);
+    }
+
+    function getInt16BE(reg: number): number {
+        pins.i2cWriteNumber(BMP180_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BMP180_I2C_ADDR, NumberFormat.Int16BE);
+    }
+
+    let AC1 = getInt16BE(0xAA)
+    let AC2 = getInt16BE(0xAC)
+    let AC3 = getInt16BE(0xAE)
+    let AC4 = getUInt16BE(0xB0)
+    let AC5 = getUInt16BE(0xB2)
+    let AC6 = getUInt16BE(0xB4)
+    let B1 = getInt16BE(0xB6)
+    let B2 = getInt16BE(0xB8)
+    let MB = getInt16BE(0xBA)
+    let MC = getInt16BE(0xBC)
+    let MD = getInt16BE(0xBE)
+    let UT = 0
+    let UP = 0
+    let T = 0
+    let P = 0
+    let X1 = 0
+    let X2 = 0
+    let X3 = 0
+    let B3 = 0
+    let B4 = 0
+    let B5 = 0
+    let B6 = 0
+    let B7 = 0
+    let _p = 0
+    let A = 0
+
+    function measure(): void {
+        setreg(0xF4, 0x2E)
+        basic.pause(6)
+        UT = getUInt16BE(0xF6)
+        setreg(0xF4, 0x34)
+        basic.pause(6)
+        UP = getUInt16BE(0xF6)
+    }
+
+    function get(): void {
+        measure()
+        X1 = Math.idiv((UT - AC6) * AC5, (1 << 15))
+        X2 = Math.idiv(MC * (1 << 11), (X1 + MD))
+        B5 = X1 + X2
+        T = Math.idiv((B5 + 8), 160)
+        B6 = B5 - 4000
+        X1 = Math.idiv((B2 * Math.idiv(B6 * B6, (1 << 12))), (1 << 11))
+        X2 = Math.idiv(AC2 * B6, (1 << 11))
+        X3 = X1 + X2
+        B3 = Math.idiv((AC1 * 4 + X3) + 2, 4)
+        X1 = Math.idiv(AC3 * B6, (1 << 13))
+        X2 = Math.idiv(B1 * Math.idiv(B6 * B6, (1 << 12)), (1 << 16))
+        X3 = Math.idiv((X1 + X2 + 2), 4)
+        B4 = Math.idiv(AC4 * (X3 + 32768), (1 << 15))
+        B7 = (UP - B3) * 50000
+        _p = Math.idiv(B7, B4) * 2
+        X1 = Math.idiv(_p, (1 << 8)) * Math.idiv(_p, (1 << 8))
+        X1 = Math.idiv((X1 * 3038), (1 << 16))
+        X2 = Math.idiv((-7357 * _p), (1 << 16))
+        P = _p + Math.idiv(X1 + X2 + 3791, 16)
+    }
+
+    /**
+     * get measure value
+     */
+    //% blockId="getMeasureValue" block="BMP180 %myValue"
+    //% weight=90 blockGap=8
+    export function measureValue(myValue: valueType): number {
+        let returnValue = 0;
+        get();
+        switch (myValue) {
+            case 1:
+                returnValue = P/100;
+                break;
+            case 2:
+                returnValue = T;
+                break;
+            case 3:
+                returnValue=Math.roundWithPrecision(44300 * (1 - Math.exp((1 / 5.256) * Math.log(P / 101325))), 2)
+                break;
+            default:
+                returnValue = 0;
+        }
+        return returnValue;
+    }
+}
